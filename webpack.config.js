3bf7aa607b85fe5ab4d50b0f551ee84843e93cc2
Ken Vud <vudken@gmail.com>
2023-04-17 13:05:51 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -72,11 +72,17 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 
-//     .enablePostCssLoader((options) => {
-//         options.postcssOptions = {
-//             config: './postcss.config.js'
-//         };
-//     })
-// ;
+    //     .enablePostCssLoader((options) => {
+    //         options.postcssOptions = {
+    //             config: './postcss.config.js'
+    //         };
+    //     })
+    // ;
+
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+        pattern: /\.(png|jpg|jpeg)$/
+    });
 
 module.exports = Encore.getWebpackConfig();
